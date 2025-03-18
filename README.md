4710448d265efbe7468c057b5228107807fb3f74
mikaxhuu <melissajunqueira44@gmail.com>
2025-03-18 00:20:15 -0300

    Código do pacote feito, só falta ajustes de descrição e links.

---
+++ README.md
@@ -0,0 +1,15 @@
+## Objetivo geral
+
+  Fazer um pacote para informar a idade do usuário a partir da sua data de nascimento.
+
+# Autora
+
+Melissa
+
+# Publicar no Test Pypi e Pypi:
+
+[Test Pypi](https://test.pypi.org/manage/account/two-factor/?next=%2Fmanage%2Fprojects%2F)
+
+[Pypi](https://pypi.org/manage/account/two-factor/?next=%2Fmanage%2Fprojects%2F)
+
+**Publicar na DIO:** [Entrega do projeto](https://web.dio.me/lab/descomplicando-a-criacao-de-pacotes-de-processamento-de-imagens-em-python/learning/aa58efe8-33dc-49fa-869f-bcdcd5eb1474)
---
+++ calcular_idade/funcionalidade/calculo_idade.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from math import floor
+
+
+def calculo_idade(data_nascimento):
+
+    anos = (datetime.now() - data_nascimento).days // 365
+    meses = floor(((datetime.now() - data_nascimento).days / 365) * 12)
+    dias = (datetime.now() - data_nascimento).days
+    
+    if anos == 1:
+        return f'Você tem: {anos} ano de idade, {meses} meses e {dias} dias de vida.'
+
+    elif anos != 1:
+        return f'Você tem: {anos} anos de idade, {meses} meses e {dias} dias de vida.'
+    
+    data_nascimento = datetime.strptime(input('Informe a data de nascimento (formato dd/mm/AAAA): '), "%d/%m/%Y")
+
+    return "Sua idade: (calculo_idade(data_nascimento)"
